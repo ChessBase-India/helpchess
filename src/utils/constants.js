f0c55bf3c623a782f97375bf8b7347d44852d0c3
Adarsh <theadxy@gmail.com>
2024-08-23 17:33:01 +0530

    cleanup

---
+++ src/utils/constants.js
@@ -0,0 +1,54 @@
+export const testimonials = [
+    {
+        name: "arjun erigaisi",
+        title: "Super Grand Master",
+        description:
+            "HelpChess is a great initiative. I personally have been involved with it a few times and it’s such a wonderful feeling that the help has reached the ones in need in a timely manner. If you are looking to help chess then do consider HelpChess.",
+        image: "/images/arjun.jpeg",
+    },
+    {
+        name: "Vidit Gujrathi",
+        title: "Super Grand Master",
+        description:
+            "I have personally seen and felt how big of an impact help chess and Chessbase India have had on the people in the chess community. Their timely help gave many talents the required resources to change their lives. I take great pride in closely knowing the lovely human beings behind this initiative. Sagar and Amruta. And if there is anyone who can be trusted to take this huge responsibility, it's them. If people want to support the cause, I'd highly recommend helpchess.org.",
+        image: "/images/vidit.jpg",
+    },
+    {
+        name: "Samay Raina",
+        title: "Chess Youtuber, Comedian",
+        description:
+            "When it comes to contributing to Indian chess, if there is anybody who comes to my mind after Vishy Anand, it is only and only ChessBase India. HelpChess OP!",
+        image: "/images/samay.jpg",
+    },
+    {
+        name: "Tanmay Bhat",
+        title: "Youtuber, Comedian",
+        description:
+            "Sagar is singularly the most passionate man I know in the Indian chess community, loved not just by fans, but admired and respected by Indian and global chess players alike; so happy to know that all the compassionate work he has done over the years is getting formalized and turning into a true moment through this foundation—do take a moment and contribute to it; when the history of Indian chess is written, nobody will be prouder than Sagar.",
+        image: "/images/tanmay.jpg",
+    },
+    {
+        name: "Humpy Koneru",
+        title: "Super Grandmaster",
+        description:
+            "HelpChess is a great initiative by ChessBase India, genuinely helping talented players across the country; most importantly, it is very transparent, and one can be assured that donated funds are used in the right way.",
+        image: "/images/humpy.jpeg",
+    },
+];
+
+export const donationCardContent = [
+    {
+        title: "Believer",
+        subtitle: "Any amount of your choice.",
+        description:
+            "Support a player to buy a laptop and learn with chess engines for more expert challenges",
+        img: "/images/believer.jpeg",
+    },
+    {
+        title: "Big Believer",
+        subtitle: "₹50K or above",
+        description:
+            "Support a player’s career long-term towards chess tournaments, travel.",
+        img: "/images/big-believer.jpeg",
+    },
+];
